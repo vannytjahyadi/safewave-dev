83b3ce763a2542d0d25786be100807b48ce27cb7
Vann <vannytjahyadi@gmail.com>
2018-02-01 20:07:06 +0700

    first commit

---
+++ resources/assets/js/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <dashboard-member></dashboard-member>
+    </div>
+</template>
+
+<script>
+    import DashboardMember from '../components/Dashboard/Dashboard.vue';
+
+    export default {
+        components: {
+            DashboardMember
+        }
+    }
+</script>
